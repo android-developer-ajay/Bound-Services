16bb700cf067dbb841e07442b60ab776e4e2fdb2
ajayqservices <ajay@qservicesit.com>
2016-09-28 23:03:29 +0530

    commit by ajay 28/9

--- app/src/main/java/com/ajay/developer/boundservices/MainActivity.java
+++ app/src/main/java/com/ajay/developer/boundservices/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (mServiceBound) {
+
                     timestampText.setText(mBoundService.getTimestamp());
                 }
             }
